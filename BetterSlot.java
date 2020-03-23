95e4b5988de7f87c2207162b72b511da60b005ca
jpmancini <59493646+jpmancini@users.noreply.github.com>
2020-03-23 01:00:52 -0400

    Add files via upload

---
+++ BetterSlot.java
@@ -0,0 +1,115 @@
+import java.util.Random;
+
+public class BetterSlot {
+	
+	public static void main(String args[]) {
+		/*BetterSlot slot = new BetterSlot();
+		int[] testAr = {3,3,3,5,6};
+		
+		int testPay = slot.payOff(testAr);
+		
+		System.out.println(testPay);*/
+	}
+	
+	public BetterSlot() {};
+	
+	static int[] pullTheLever() {
+		int[] nums = new int[5];
+		
+		for(int i = 0; i < 5; i++) {
+			Random r = new Random();
+			nums[i] = r.nextInt(50) + 1;
+		}
+		
+		return nums;
+	}
+	
+	int payOff( int[] values ) {
+		int payout = 0;
+		int[] counts = {0,0,0,0,0};
+		
+		boolean rule1 = false, rule2 = false, rule4 = false, rule5 = false;
+		boolean rule6 = false, rule7 = false, rule8 = false;
+		
+		
+		
+		for(int i = 0; i < 5; i++) {
+			
+			double d = values[i];
+			double root = Math.sqrt(d);
+			if(root % 1 == 0) {
+				rule6 = true;
+			}
+			
+			if(values[i] == 42) {
+				rule7 = true;
+			}
+			
+			double valueLog = Math.log(values[i]);
+			double baseTwo = valueLog / Math.log(2);
+					
+			//if the logbase2 of values[i] is a whole number => rule8 = true
+			if (baseTwo % 1 == 0) {
+				rule8 = true;
+			}
+				
+			for(int j = 0; j < 5; j++) {
+				if(values[j] == values[i]) {
+					counts[i]++;
+				}
+			}
+			
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			//System.out.println(counts[i]);
+			if(counts[i] == 5) {
+				//System.out.println("rule1");
+				rule1 = true;
+			}
+			if(counts[i] == 4) {
+				//System.out.println("rule2");
+				rule2 = true;
+			}
+			if(counts[i] == 3) {				
+				//System.out.println("rule4");
+				rule4 = true;
+			}
+			if(counts[i] == 2) {				
+				//System.out.println("rule5");
+				rule5 = true;
+			}
+		}
+		
+		if(rule1 == true) {
+			payout += 1000000;
+		}
+		else if(rule2 == true) {
+			payout += 10000;
+		}
+		else if(rule4 == true && rule5 == true) {
+			payout += 5000;
+		}
+		else if(rule4 == true) {
+			payout += 100;
+		}
+		else if(rule5 == true) {
+			payout += 10;
+		}
+		
+		if(rule6 == true) {
+			payout += 7;
+		}
+		if(rule7 == true) {
+			payout += 2;
+		}
+		if(rule8 == true) {
+			payout += 3;
+		}
+		
+		return payout;
+		
+	}
+	
+}
+
---
+++ TestSlipperySlot.java
@@ -0,0 +1,367 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class TestSlipperySlot {
+	BetterSlot slot = new BetterSlot();
+	
+	//test none
+	@Test
+	public void testRuleNone() {
+		int[] nums = new int[] {3,5,6,7,10}; 
+		
+		assertEquals(slot.payOff(nums), 0, "failed testRuleNone()");
+	}
+	
+	//test rule1
+	@Test
+	public void testRuleOne() {
+		int[] nums = new int[] {3,3,3,3,3}; 
+		
+		assertEquals(slot.payOff(nums), 1000000, "failed testRuleOne()");
+	}
+	
+	//test rule1&6
+	@Test
+	public void testRuleOneSix() {
+		int[] nums = new int[] {9,9,9,9,9}; 
+		
+		assertEquals(slot.payOff(nums), 1000007, "failed testRuleOneSix()");
+	}
+	
+	//test rule1&7
+	@Test
+	public void testRuleOneSeven() {
+		int[] nums = new int[] {42,42,42,42,42}; 
+		
+		assertEquals(slot.payOff(nums), 1000002, "failed testRuleOneSeven()");
+	}
+	
+	//test rule1&8
+	@Test
+	public void testRuleOneEight() {
+		int[] nums = new int[] {2,2,2,2,2}; 
+		
+		assertEquals(slot.payOff(nums), 1000003, "failed testRuleOneEight()");
+	}
+	
+	//test rule1&6&8
+	@Test
+	public void testRuleOneSixEight() {
+		int[] nums = new int[] {4,4,4,4,4}; 
+		
+		assertEquals(slot.payOff(nums), 1000010, "failed testRuleOneSixEight()");
+	}
+	
+	//test rule2
+	@Test
+	public void testRuleTwo() {
+		int[] nums = new int[] {3,3,3,3,5}; 
+		
+		assertEquals(slot.payOff(nums), 10000, "failed testRuleTwo()");
+	}
+	
+	//test rule2&6
+	@Test
+	public void testRuleTwoSix() {
+		int[] nums = new int[] {3,3,3,3,9}; 
+		
+		assertEquals(slot.payOff(nums), 10007, "failed testRuleTwoSix()");
+	}
+	
+	//test rule2&7
+	@Test
+	public void testRuleTwoSeven() {
+		int[] nums = new int[] {3,3,3,3,42}; 
+		
+		assertEquals(slot.payOff(nums), 10002, "failed testRuleTwoSeven()");
+	}
+	
+	//test rule2&8
+	@Test
+	public void testRuleTwoEight() {
+		int[] nums = new int[] {3,3,3,3,2}; 
+		
+		assertEquals(slot.payOff(nums), 10003, "failed testRuleTwoEight()");
+	}
+	
+	//test rule2&6&7
+	@Test
+	public void testRuleTwoSixSeven() {
+		int[] nums = new int[] {9,9,9,9,42}; 
+		
+		assertEquals(slot.payOff(nums), 10009, "failed testRuleTwoSixSeven()");
+	}
+	
+	//test rule2&6&8
+	@Test
+	public void testRuleTwoSixEight() {
+		int[] nums = new int[] {9,9,9,9,2}; 
+		
+		assertEquals(slot.payOff(nums), 10010, "failed testRuleTwoSixEight()");
+	}
+	
+	//test rule2&7&8
+	@Test
+	public void testRuleTwoSevenEight() {
+		int[] nums = new int[] {42,42,42,42,2}; 
+		
+		assertEquals(slot.payOff(nums), 10005, "failed testRuleTwoSevenEight()");
+	}
+	
+	//test rule2&6&7&8
+	@Test
+	public void testRuleTwoSixSevenEight() {
+		int[] nums = new int[] {4,4,4,4,42}; 
+		
+		assertEquals(slot.payOff(nums), 10012, "failed testRuleTwoSixSevenEight()");
+	}
+	
+	//test rule3
+	@Test
+	public void testRuleThree() {
+		int[] nums = new int[] {3,3,3,5,5}; 
+		
+		assertEquals(slot.payOff(nums), 5000, "failed testRuleThree()");
+	}
+	
+	//test rule3&6
+	@Test
+	public void testRuleThreeSix() {
+		int[] nums = new int[] {3,3,3,9,9}; 
+		
+		assertEquals(slot.payOff(nums), 5007, "failed testRuleThreeSix()");
+	}
+	
+	//test rule3&7
+	@Test
+	public void testRuleThreeSeven() {
+		int[] nums = new int[] {3,3,3,42,42}; 
+		
+		assertEquals(slot.payOff(nums), 5002, "failed testRuleThreeSeven()");
+	}
+	
+	//test rule3&8
+	@Test
+	public void testRuleThreeEight() {
+		int[] nums = new int[] {3,3,3,2,2}; 
+		
+		assertEquals(slot.payOff(nums), 5003, "failed testRuleThreeEight()");
+	}
+	
+	//test rule3&6&7
+	@Test
+	public void testRuleThreeSixSeven() {
+		int[] nums = new int[] {9,9,9,42,42}; 
+		
+		assertEquals(slot.payOff(nums), 5009, "failed testRuleThreeSixSeven()");
+	}
+	
+	//test rule3&6&8
+	@Test
+	public void testRuleThreeSixEight() {
+		int[] nums = new int[] {9,9,9,2,2}; 
+		
+		assertEquals(slot.payOff(nums), 5010, "failed testRuleThreeSixEight()");
+	}
+	
+	//test rule3&7&8
+	@Test
+	public void testRuleThreeSevenEight() {
+		int[] nums = new int[] {42,42,42,2,2}; 
+		
+		assertEquals(slot.payOff(nums), 5005, "failed testRuleThreeSevenEight()");
+	}
+	
+	//test rule3&6&7&8
+	@Test
+	public void testRuleThreeSixSevenEight() {
+		int[] nums = new int[] {4,4,4,42,42}; 
+		
+		assertEquals(slot.payOff(nums), 5012, "failed testRuleThreeSixSevenEight()");
+	}
+	
+	//test rule4
+	@Test
+	public void testRuleFour() {
+		int[] nums = new int[] {3,3,3,5,6}; 
+		
+		assertEquals(slot.payOff(nums), 100, "failed testRuleFour()");
+	}
+	
+	//test rule4&6
+	@Test
+	public void testRuleFourSix() {
+		int[] nums = new int[] {3,3,3,5,9}; 
+		
+		assertEquals(slot.payOff(nums), 107, "failed testRuleFourSix()");
+	}
+	
+	//test rule4&7
+	@Test
+	public void testRuleFourSeven() {
+		int[] nums = new int[] {3,3,3,5,42}; 
+		
+		assertEquals(slot.payOff(nums), 102, "failed testRuleFourSeven()");
+	}
+		
+	//test rule4&8
+	@Test
+	public void testRuleFourEight() {
+		int[] nums = new int[] {3,3,3,5,2}; 
+		
+		assertEquals(slot.payOff(nums), 103, "failed testRuleFourEight()");
+	}
+	
+	//test rule4&6&7
+	@Test
+	public void testRuleFourSixSeven() {
+		int[] nums = new int[] {3,3,3,9,42}; 
+		
+		assertEquals(slot.payOff(nums), 109, "failed testRuleFourSixSeven()");
+	}
+	
+	//test rule4&6&8
+	@Test
+	public void testRuleFourSixEight() {
+		int[] nums = new int[] {3,3,3,9,2}; 
+		
+		assertEquals(slot.payOff(nums), 110, "failed testRuleFourSixEight()");
+	}
+	
+	//test rule4&7&8
+	@Test
+	public void testRuleFourSevenEight() {
+		int[] nums = new int[] {3,3,3,42,2}; 
+		
+		assertEquals(slot.payOff(nums), 105, "failed testRuleFourSevenEight()");
+	}
+	
+	//test rule4&6&7&8
+	@Test
+	public void testRuleFourSixSevenEight() {
+		int[] nums = new int[] {3,3,3,4,42}; 
+		
+		assertEquals(slot.payOff(nums), 112, "failed testRuleFourSixSevenEight()");
+	}
+	
+	//test rule5
+	@Test
+	public void testRuleFive() {
+		int[] nums = new int[] {3,3,5,6,7}; 
+		
+		assertEquals(slot.payOff(nums), 10, "failed testRuleFive()");
+	}
+	
+	//test rule5&6
+	@Test
+	public void testRuleFiveSix() {
+		int[] nums = new int[] {3,3,5,6,9}; 
+		
+		assertEquals(slot.payOff(nums), 17, "failed testRuleFiveSix()");
+	}
+	
+	//test rule5&7
+	@Test
+	public void testRuleFiveSeven() {
+		int[] nums = new int[] {3,3,5,6,42}; 
+		
+		assertEquals(slot.payOff(nums), 12, "failed testRuleFiveSeven()");
+	}
+		
+	//test rule5&8
+	@Test
+	public void testRuleFiveEight() {
+		int[] nums = new int[] {3,3,5,6,2}; 
+		
+		assertEquals(slot.payOff(nums), 13, "failed testRuleFiveEight()");
+	}
+	
+	//test rule5&6&7
+	@Test
+	public void testRuleFiveSixSeven() {
+		int[] nums = new int[] {3,3,5,9,42}; 
+		
+		assertEquals(slot.payOff(nums), 19, "failed testRuleFiveSixSeven()");
+	}
+	
+	//test rule5&6&8
+	@Test
+	public void testRuleFiveSixEight() {
+		int[] nums = new int[] {3,3,5,9,2}; 
+		
+		assertEquals(slot.payOff(nums), 20, "failed testRuleFiveSixEight()");
+	}
+	
+	//test rule5&7&8
+	@Test
+	public void testRuleFiveSevenEight() {
+		int[] nums = new int[] {3,3,5,42,2}; 
+		
+		assertEquals(slot.payOff(nums), 15, "failed testRuleFiveSevenEight()");
+	}
+	
+	//test rule5&6&7&8
+	@Test
+	public void testRuleFiveSixSevenEight() {
+		int[] nums = new int[] {3,3,5,4,42}; 
+		
+		assertEquals(slot.payOff(nums), 22, "failed testRuleFiveSixSevenEight()");
+	}
+	
+	//test rule6
+	@Test
+	public void testRuleSix() {
+		int[] nums = new int[] {3,5,6,7,9}; 
+		
+		assertEquals(slot.payOff(nums), 7, "failed testRuleSix()");
+	}
+	
+	//test rule7
+	@Test
+	public void testRuleSeven() {
+		int[] nums = new int[] {3,5,6,7,42}; 
+		
+		assertEquals(slot.payOff(nums), 2, "failed testRuleSeven()");
+	}
+	
+	//test rule8
+	@Test
+	public void testRuleEight() {
+		int[] nums = new int[] {3,5,6,7,2}; 
+		
+		assertEquals(slot.payOff(nums), 3, "failed testRuleEight()");
+	}
+	
+	//test rule6&7
+	@Test
+	public void testRuleSixSeven() {
+		int[] nums = new int[] {3,5,6,9,42};
+		
+		assertEquals(slot.payOff(nums), 9, "failed testRuleSixSeven()");
+	}
+	
+	//test rule6&8
+	@Test
+	public void testRuleSixEight() {
+		int[] nums = new int[] {3,5,6,9,2};
+		
+		assertEquals(slot.payOff(nums), 10, "failed testRuleSixEight()");
+	}
+	
+	//test rule7&8
+	@Test
+	public void testRuleSevenEight() {
+		int[] nums = new int[] {3,5,6,42,2};
+		
+		assertEquals(slot.payOff(nums), 5, "failed testRuleSevenEight()");
+	}
+	
+	//test rule6&7&8
+	@Test
+	public void testRuleSixSevenEight() {
+		int[] nums = new int[] {3,5,9,42,2};
+		
+		assertEquals(slot.payOff(nums), 12, "failed testRuleSixSevenEight()");
+	}
+}
